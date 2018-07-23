20726474947ef045863bf760717060e6b3429212
Bolimbo <carlo.cappellini@yahoo.com>
2018-07-22 22:31:09 -0500

    imported Forms module in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -7,7 +8,8 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FormsModule
   ],
   imports: [
     BrowserModule
